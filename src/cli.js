0f625353b5adbd8008aa0e261930dc2c55b66c19
Temirlan Kozhagulov <kozhagulovt@gmail.com>
2024-04-04 23:42:04 +0500

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readLineSync from 'readline-sync';
 
-export const greetings = () => {
-    const name = readLineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+const greetings = () => {
+  const name = readLineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default greetings;
